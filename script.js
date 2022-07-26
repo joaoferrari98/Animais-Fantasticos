289e97e2539e88e4508f83a56cf170e73bc1fff5
joao victor ferrari <ferrarimello@outlook.com>
2022-07-26 13:57:08 -0300

    a

--- script.js
+++ script.js
@@ -1,19 +1,57 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add("ativo");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
 
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove("ativo");
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add("ativo");
   }
+}
+initTabNav();
 
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener("click", () => {
-      activeTab(index);
+function innitAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  if (accordionList.length) {
+    accordionList[0].classList.add("ativo");
+    accordionList[0].nextElementSibling.classList.add("ativo");
+
+    function activeAccordion() {
+      this.classList.add("ativo");
+      this.nextElementSibling.classList.add("ativo");
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
     });
+  }
+}
+innitAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
   });
 }
+initScrollSuave();
